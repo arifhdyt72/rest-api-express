d5b58e38b8b0faef03368985aeb1e0e53240351d
Arif Hidayat <arif.hidayat_aam@AM-074477.HOSDG>
2022-10-20 11:07:18 +0700

    crud mahasiswa

--- controller.js
+++ controller.js
@@ -6,3 +6,69 @@ let connection = require('./koneksi');
 exports.index = function(req, res){
     response.ok("My Controller Router has been running.", res);
 }
+
+exports.getAllMhs = function(req, res) {
+    connection.query("SELECT * FROM mahasiswa", function(error, rows, fields){
+        if(error){
+            console.log(error);
+        } else {
+            response.ok(rows, res);
+        }
+    });
+}
+
+exports.getDetailMhsById = function(req, res) {
+    let id = req.params.id;
+    connection.query("SELECT * FROM mahasiswa WHERE id_mahasiswa = ?", [id],
+    function(error, rows, fields){
+        if(error){
+            console.log(error);
+        }else{
+            response.ok(rows, res);
+        }
+    });
+}
+
+exports.addMhs = function(req, res) {
+    let nim = req.body.nim;
+    let nama = req.body.nama;
+    let jurusan = req.body.jurusan;
+
+    connection.query("INSERT INTO mahasiswa(nim, nama, jurusan) VALUES(?,?,?)", [nim, nama, jurusan],
+        function(error, rows, fields){
+            if (error){
+                console.log(error);
+            }else{
+                response.ok("Successfully add data mhs!", res);
+            }
+        }
+    );
+}
+
+exports.updateMhs = function(req, res){
+    let id = req.body.id;
+    let nim = req.body.nim;
+    let nama = req.body.nama;
+    let jurusan = req.body.jurusan;
+
+    connection.query("UPDATE mahasiswa SET nim=?, nama=?, jurusan=? WHERE id_mahasiswa=?", [nim,nama,jurusan,id],
+    function(error, rows, fields){
+        if(error){
+            console.log(error);
+        }else{
+            response.ok('Successfully to update data!', res)
+        }
+    });
+}
+
+exports.deleteMhs = function(req, res){
+    let id = req.params.id;
+    connection.query('DELETE FROM mahasiswa WHERE id_mahasiswa = ?', [id],
+    function(error, rows, fields) {
+        if(error){
+            console.log(error)
+        }else{
+            response.ok("Successfully to delete data!", res)
+        }
+    });
+}
--- router.js
+++ router.js
@@ -3,5 +3,10 @@
 module.exports = function(app){
     let jsonku = require('./controller');
 
-    app.route('/').get(jsonku.index)
+    app.route('/').get(jsonku.index);
+    app.route('/get-mhs').get(jsonku.getAllMhs);
+    app.route('/get-mhs/:id').get(jsonku.getDetailMhsById);
+    app.route('/add-mhs').post(jsonku.addMhs);
+    app.route('/update-mhs').put(jsonku.updateMhs);
+    app.route('/delete-mhs/:id').delete(jsonku.deleteMhs);
 }
